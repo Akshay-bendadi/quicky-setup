5d83d5d05560bb74a422fa2b67fc2f3113ce1187
Akshay Bendadi <116073822+Akshay-bendadi@users.noreply.github.com>
2025-10-05 11:35:27 +0530

    Delete dist directory

--- dist/src/main.js
+++
@@ -1,6 +0,0 @@
-import { askQuestions } from './prompts.js';
-import { scaffoldProject } from './scaffolder.js';
-export async function main() {
-    const answers = await askQuestions();
-    await scaffoldProject(answers);
-}
--- dist/src/scaffolders/index.js
+++
@@ -1,3 +0,0 @@
-export * from './apiService.js';
-export * from './reduxStore.js';
-// Export other scaffolders as they are created
